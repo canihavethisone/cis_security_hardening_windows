2c336d2683a238b1b22b584dce808126092762df
canihavethisone <spam54321@hotmail.com>
2025-05-27 12:35:36 +1000

    Refactored custom windows fact

--- lib/facter/windows_custom.rb
+++ lib/facter/windows_custom.rb
@@ -1,58 +1,51 @@
 Facter.add('windows') do
+  # Only run this fact on Windows systems
   confine kernel: 'windows'
 
-  # Initialise variables
+  require 'win32/registry'
+  require 'win32ole'
+
+  # Default values in case retrieval fails
   windows_currentbuildnumber = 'unknown'
-  windows_displayversion = 'unknown'
-  windows_releaseid = 'unknown'
-  windows_version = 'unknown'
+  windows_displayversion     = 'unknown'
+  windows_releaseid          = 'unknown'
+  windows_version            = nil
 
   begin
-    if RUBY_PLATFORM.downcase.include?('mswin') || RUBY_PLATFORM.downcase.include?('mingw32')
-      require 'win32/registry'
-      require 'win32ole'
+    # Check if running on a supported Windows platform
+    if RUBY_PLATFORM =~ /mswin|mingw32/i
+      # Connect to WMI to get OS details
       wmi = WIN32OLE.connect('winmgmts:\\\\.\\root\\cimv2')
+      windows_version = wmi.ExecQuery('SELECT Caption, BuildNumber FROM Win32_OperatingSystem').each.first
 
-      # Retrieve operatingsystem hash from WMI as registry values were not incremented by Microsoft
-      windows_version = wmi.ExecQuery('SELECT * FROM Win32_operatingsystem').each.first
-
-      # Get values for build, display and releaseid from registry
-      Win32::Registry::HKEY_LOCAL_MACHINE.open('Software\Microsoft\Windows NT\CurrentVersion') do |reg|
-        reg.each do |name, _type, data|
-          if name.eql?('CurrentBuildNumber')
-            windows_currentbuildnumber = data
-          end
-          if name.eql?('DisplayVersion')
-            windows_displayversion = data
-          end
-          if name.eql?('ReleaseId')
-            windows_releaseid = data
-          end
-        end
+      # Open registry path to retrieve build-related versioning info
+      Win32::Registry::HKEY_LOCAL_MACHINE.open('Software\\Microsoft\\Windows NT\\CurrentVersion') do |reg|
+        windows_currentbuildnumber = reg['CurrentBuildNumber'] rescue 'unknown'
+        windows_displayversion     = reg['DisplayVersion']     rescue 'unknown'
+        windows_releaseid          = reg['ReleaseId']          rescue 'unknown'
       end
     end
-  rescue
-    nil
+  rescue => e
+    # Log errors without failing the fact
+    Facter.debug("windows custom fact error: #{e.class}: #{e.message}")
   end
 
-  # Windows_display_version determined by currentbuildnumber value to manage change from 20h2 to 21h1 onward
-  windows_display_version = if windows_currentbuildnumber >= '19043'
-                              windows_displayversion
-                            else
-                              windows_releaseid
-                            end
+  # Microsoft moved from ReleaseId to DisplayVersion starting around build 19043
+  windows_display_version = windows_currentbuildnumber >= '19043' ? windows_displayversion : windows_releaseid
 
   setcode do
-    # Create hash for values
-    value = {}
-    # Get the product name but remove leading 'Microsoft'
-    value['product_name']    = windows_version.Caption.split(' ')[1..-1].join(' ')
-    value['release']         = windows_version.Caption.split(' ')[2]
-    value['edition_id']      = windows_version.Caption.split(' ')[3]
-    value['display_version'] = windows_display_version
-    value['build_number']    = windows_version.BuildNumber
-
-    # Return hash
-    value
+    if windows_version
+      # Safely split the caption string to extract product and edition info
+      caption_parts = windows_version.Caption.to_s.split(' ')
+      {
+        'product_name'    => caption_parts[1..].join(' '),              # Strip "Microsoft" prefix
+        'release'         => caption_parts[2] || 'unknown',             # e.g. "10"
+        'edition_id'      => caption_parts[3] || 'unknown',             # e.g. "Pro"
+        'display_version' => windows_display_version,                   # e.g. "22H2"
+        'build_number'    => windows_version.BuildNumber                # e.g. "19045"
+      }
+    else
+      {}
+    end
   end
 end
